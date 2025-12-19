6c63e60caf0c683cc71db5c333c6f86d25269dc1
YagiSheepOp <goatnasamgho@gmail.com>
2025-12-19 17:01:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
-import json
 import os
+import json
 
 intents = discord.Intents.default()
 intents.members = True
@@ -9,30 +9,28 @@ intents.presences = True
 
 bot = commands.Bot(command_prefix="!gcart ", intents=intents)
 
-# Load data
+# ---------------- LOAD DATA ---------------- #
 with open("data.json", "r") as f:
     DATA = json.load(f)
 
 REQUIRED_STATUS = DATA["required_status"].lower()
 
 
-# ---------------- STATUS CHECK SYSTEM ---------------- #
-
+# ---------------- STATUS CHECK ---------------- #
 def has_required_status(member):
     try:
         if member.activity and member.activity.state:
             status_text = str(member.activity.state).lower()
-            return REQUIRED_STATUS in status_text  
+            return REQUIRED_STATUS in status_text
         return False
     except:
         return False
 
 
-# --------------- EMBED STATUS ERROR MESSAGE ---------------- #
-
+# ---------------- STATUS ERROR EMBED ---------------- #
 async def send_status_error(ctx):
     embed = discord.Embed(
         title="❌ Required Status Missing",
         description=f"""
-<a:animatedarrowgreen:1450811653552607296> Use This Status to Unlock Free Gen:
+<a:animatedarrowgreen:1450811653552607296> Use This Status to Access Gen System:
 
